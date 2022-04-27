3ab0605e52d88ce9b5fe4b2b19ad0cc6aa1a61b6
Anna <zholnerchikanna0303@gmail.com>
2022-04-27 15:16:52 +0300

    add loader

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,49 +1,52 @@
 import React from "react";
 import s from "./Users.module.css";
-import * as axios from 'axios'
 import UserPhoto from '../../assets/images/user.png'
 
-class Users extends React.Component {
 
-  componentDidMount() {
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(response => {
-        this.props.setUsers(response.data.items)
-      })
+const Users = (props) => {
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+
+  let pages = []
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i)
   }
-  render() {
-    return <div>
-      {
-        this.props.users.map(u => <div key={u.id}>
-          <span>
-            <div>
-              <img src={u.photos.small != null ? u.photos.small : UserPhoto} className={s.userPhoto} alt="user_photo" />
-            </div>
-            <div>
-              {u.subscribed ?
-                <button onClick={() => {
-                  this.props.unsubscribe(u.id)
-                }}>Unsubscribe</button> :
-                <button onClick={() => {
-                  this.props.subscribe(u.id)
-                }}>Subscribe</button>}
+  return < div >
+    <div>
+      {pages.map(p => {
+        return <span className={props.currentPage === p && s.selectedPage}
+          onClick={(e) => { props.onPageChanged(p) }} > {p}</span>
+      })}
+    </div>
+    {
+      props.users.map(u => <div key={u.id}>
+        <span>
+          <div>
+            <img src={u.photos.small != null ? u.photos.small : UserPhoto}
+              className={s.userPhoto} alt="user_photo" />
+          </div>
+          <div>
+            {u.subscribed ?
+              <button onClick={() => {
+                props.unsubscribe(u.id)
+              }}>Unsubscribe </button> :
+              <button onClick={() => {
+                props.subscribe(u.id)
+              }}>Subscribe </button>}
 
-            </div>
+          </div>
+        </span>
+        <span>
+          <span>
+            <div>{u.name}</div>
+            <div>{u.status}</div>
           </span>
           <span>
-            <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
-            </span>
-            <span>
-              {/* <div>{u.location.country}</div> */}
-              {/* <div>{u.location.city}</div> */}
-            </span>
+            {/* <div>{u.location.country}</div> */}
+            {/* <div>{u.location.city}</div> */}
           </span>
-        </div>)
-      }
-    </div >
-  }
+        </span>
+      </div>)
+    }
+  </div >
 }
-
 export default Users
