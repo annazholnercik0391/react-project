8e74f1fa8d87bdbd6fd648085fd904dd054f4364
Anna <zholnerchikanna0303@gmail.com>
2022-04-29 15:11:22 +0300

    login, authReducer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
-      <img className={s.logo} src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Logo_TV_2015.svg/1200px-Logo_TV_2015.svg.png' alt='logo' />
+      <img className={s.logo}
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Logo_TV_2015.svg/1200px-Logo_TV_2015.svg.png' alt='logo' />
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login
+          : <NavLink to={'/login'}>Login</NavLink>}
+
+      </div>
     </header>
   )
 }
