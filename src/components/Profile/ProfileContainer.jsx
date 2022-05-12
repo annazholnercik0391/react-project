5da52930e268c49292cd2cba450ff9bdb8855bea
Anna <zholnerchikanna0303@gmail.com>
2022-05-12 10:38:30 +0300

    ProfileStatus and local state

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -31,7 +31,6 @@ export default compose(
     getUserProfile
   }),
   withRouter,
-  withAuthRedirect
 )(ProfileContainer)
 
 
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import s from "./ProfileInfo.module.css"
 import Preloader from "../../common/Preloader/Preloader"
 import noPhoto from "../../../assets/images/no-photo.png"
-
+import ProfileStatus from './ProfileStatus'
 const ProfileInfo = (props) => {
 
   if (!props.profile) {
     return <Preloader />
   }
-
   return (
     <div>
-      <div>
+      {/* <div>
         <img src='https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg' alt="main_picture" />
-      </div>
+      </div> */}
+      <ProfileStatus status='Hello my friends' />
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large != null ? props.profile.photos.large : noPhoto} alt="#"></img>
         <div >
